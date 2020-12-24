d001da36763a90c5828bedca1183d462042f52fe
Easton Potokar <contagon6@gmail.com>
2020-12-24 10:42:57 -0700

    Getting started with tests

--- tests/test_ldsnotes.py
+++ tests/test_ldsnotes.py
@@ -3,21 +3,8 @@
 """Tests for `ldsnotes` package."""
 
 import pytest
+import os
+from ldsnotes import Notes
 
-
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
-    pass
-
-
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
-    pass
+def test_login():
+    n = Notes(os.environ['USERNAME'], os.environ['PASSWORD'])
